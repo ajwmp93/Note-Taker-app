546b52b90a447a6fd6c24f5e8b257be27c390d16
Ashley Wright <ashleywright@iMac.local>
2024-07-22 16:23:10 -0400

    debugged my code so it writes to the db.json file

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const uniqid = require('uniqid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dbFilePath = path.join(__dirname, './db/db.json');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+if (!fs.existsSync(dbFilePath)) {
+    fs.writeFileSync(dbFilePath, '[]', 'utf8');
+}
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -19,17 +24,20 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+    const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+    const newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    };
+    const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
 
-    newNote.id = uniqid();
     notes.push(newNote);
 
-    fs.writeFileSync('./db.json', JSON.stringify(notes, null, 2));
+    fs.writeFileSync(dbFilePath, JSON.stringify(notes, null, 2));
 
     res.json(newNote);
 });
