5a63a52f34c7dcdbda99f728be027f0ef68d957b
Ashley Wright <ashleywright@iMac.local>
2024-07-22 15:41:25 -0400

    added in my app.use expressions and began adding my app.get expressions

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
