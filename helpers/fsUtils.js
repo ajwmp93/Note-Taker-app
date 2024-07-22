8095d94804aef534abee5187b7509afe9b75885f
Ashley Wright <ashleywright@iMac.local>
2024-07-22 16:34:40 -0400

    wrote out the read data and write data functions to hopefully write the cards

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -3,3 +3,25 @@ const path = require('path');
 
 const dbFilePath = path.join(__dirname, './db/db.json');
 
+const readDataFromFile = () => {
+    try {
+        const data = fs.readFileSync(dbFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading data from db.json:', error);
+        return[];
+    }
+};
+
+const writeDataToFile = (data) => {
+    try {
+        fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing data to db.json:', error);
+    }
+};
+
+module.exports = {
+    readDataFromFile,
+    writeDataToFile,
+};
