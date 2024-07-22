257c4d22448c1e441643e50db0ee3ee43be03597
Ashley Wright <ashleywright@iMac.local>
2024-07-22 15:44:30 -0400

    added app.get instructions

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+});
+
