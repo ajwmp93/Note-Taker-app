b3f28c7094aca1b6ed4e58f534453dd355157400
Ashley Wright <ashleywright@iMac.local>
2024-07-22 15:49:47 -0400

    added my app.post instructions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,5 +20,17 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+
+    newNote.id = uniqid();
+    notes.push(newNote);
+
+    fs.writeFileSync('./db.json', JSON.stringify(notes, null, 2));
+
+    res.json(newNote);
 });
 
